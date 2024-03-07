e411609bfe5915586daa7a3910c026301bb3a91f
Allex Thiago <allexthiagogatinho502@gmail.com>
2024-03-07 19:54:04 -0300

    docs: Comment the twoOpt function

--- src/utils/calculateDistance.js
+++ src/utils/calculateDistance.js
@@ -1,8 +1,8 @@
-const calculateDistance = (point1, point2) => Math
+const calculateEuclidianDistance = (point1, point2) => Math
   .sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
 
 const calculateTotalDistance = (route) => route.reduce((acc, curr, index) => (
-  index === route.length - 1 ? acc : acc + calculateDistance(curr, route[index + 1])
+  index === route.length - 1 ? acc : acc + calculateEuclidianDistance(curr, route[index + 1])
 ), 0);
 
-module.exports = { calculateDistance, calculateTotalDistance };
+module.exports = { calculateEuclidianDistance, calculateTotalDistance };
--- src/utils/nearestNeighbor.js
+++ src/utils/nearestNeighbor.js
@@ -1,4 +1,4 @@
-const { calculateDistance } = require("./calculateDistance");
+const { calculateEuclidianDistance } = require("./calculateDistance");
 
 module.exports = (points) => {
   const indices = Array.from({ length: points.length }, (_, i) => i);
@@ -13,7 +13,7 @@ module.exports = (points) => {
     for (const index of indices) { // vai rodar um loop para cada indice do array de pontos
       const isTheSamePoint = points[index].id === actualPoint.id;
       if (!visited.has(index) && !isTheSamePoint) { // Se o ponto não visitado ainda e não for igual ao ponto de indice igual a index
-        const distance = calculateDistance(actualPoint, points[index]); // Calcula a distância entre o ponto atual e o ponto que estamos analizando
+        const distance = calculateEuclidianDistance(actualPoint, points[index]); // Calcula a distância entre o ponto atual e o ponto que estamos analizando
         if (distance < minDistance) { // Se a distância for menor que a distância mínima do loop while atual
           minDistance = distance; // Salva ela como nova distância mínima
           nearestNeighbor = index; // Salva o indice como o indice do vizinho mais próximo
